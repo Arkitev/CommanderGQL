16483c6a4850b0303b04dcad3aea0e393eac808a
PBI\AR010GO <artur.gora@pb.com>
2021-11-19 13:35:00 +0100

    Init

---
+++ CommanderGQL/GraphQL/Subscription.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommanderGQL.Models;
+using HotChocolate;
+using HotChocolate.Types;
+
+namespace CommanderGQL.GraphQL
+{
+    public class Subscription
+    {
+        [Subscribe]
+        [Topic]
+        public Platform OnPlatformAdded([EventMessage] Platform platform) => platform;
+    }
+}
